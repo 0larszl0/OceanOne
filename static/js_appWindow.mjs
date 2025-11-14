e60add84727fd3596131478e58ee4e8229cbeaea
0larszl0 <oliver.winhammar@gmail.com>
2025-11-14 10:10:07 +0000

    Windows can now toggle on app icon press, note other mechanics like drag, close and resize are not yet connected.

--- static/js_appWindow.mjs
+++ static/js_appWindow.mjs
@@ -1,18 +1,18 @@
+/* --- Window Factory --- */
+
 /**
  * Create a window with a given context to it.
  * @param {string} ctx The context for this new window.
+ * @param {HTMLDivElement} window_group The parent that should child this new window object.
  */
-async function createWindow(ctx) {
-    let window_container = document.getElementById("window-container");
+async function createWindow(ctx, window_group) {
     let new_window = document.createElement("div");
 
     switch (ctx) {
         case 'email':
-            // Check if a group for emails has been made and if so group the new window inside it, if not create that group and add the new window inside it.
-
             // send a fetch for the window data with the given context, and then add that data to the new window.
             await bodiedFetch(
-                '/get-window',
+                "/get-window",
                 {"ctx": ctx},
                 add_src, new_window
             );
@@ -20,19 +20,40 @@ async function createWindow(ctx) {
             break;
     }
 
-    window_container.appendChild(new_window);
+    new_window.classList = "app-window hidden"
+    window_group.appendChild(new_window);
 }
 
 /**
  * Adds the windowHTML within the src JSON, to the forwarded element.
- * @param {src} json The json response from the server, via bodiedFetch
- * @param {element} Element The new window element that was created.
+ * @param {json} src The json response from the server, via bodiedFetch
+ * @param {HTMLDivElement} element The new window element that was created.
  */
 function add_src(src, element) {
     element.innerHTML = src["windowHTML"];
 }
 
 
+/* --- Window Visibility --- */
+async function toggleWindow(ctx) {
+    let window_container = document.getElementById("window-container");
+    let window_group = document.getElementById(`${ctx}-group`);
+
+    if (window_group == null) {  // if there is no window group for this context
+        // then create one-
+        window_group = document.createElement("div");
+        window_group.id = `${ctx}-group`;
+        window_group.classList.add("window-group");
+
+        window_container.appendChild(window_group);  // append the new group into the window container
+        await createWindow(ctx, window_group);  // wait till the new window has been created before continuing.
+    }
+
+    if (window_group.childElementCount == 1) {  // if there's only one window for this group
+        // toggle the visibility of that window
+        window_group.firstChild.classList.toggle("hidden");
+    }
+}
 
 
 
